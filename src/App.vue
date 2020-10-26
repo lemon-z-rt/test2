5aa4a59fac370d411ce1b00983a2b975e897f2d9
lemon-z-rt <577002370@qq.com>
2020-10-26 21:54:21 +0800

    第一次

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+   
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+import { Indicator } from 'mint-ui';
+
+export default {
+  name: 'App',
+  components: {
+   
+  },
+  data() {
+    return {
+      timer:''
+    }
+  },
+  methods: {
+    loading(){
+    Indicator.open({
+     text: '加载中...',
+    spinnerType: 'fading-circle'
+        })
+       },
+    },
+    mounted() {
+     this.loading()
+    this.timer=setTimeout(()=>{
+         Indicator.close();
+    },500)
+ },   
+}
+ 
+</script>
+
+<style>
+/* 将公共的css引入进来 */
+@import "./fonts/iconfont.css";
+@import "./style/common.css";
+</style>
